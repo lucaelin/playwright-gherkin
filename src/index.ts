ce233870bf087b543d69ebec3dd02b7e1d60562b
Luca Haneklau <git@luca.lsys.ac>
2023-02-19 20:25:32 +0100

    fix entrypoints

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,14 @@ import {readFile, writeFile} from 'node:fs/promises';
 import glob from 'glob';
 import {generateCode} from './generate.js';
 import {createHash} from 'node:crypto';
+import { parseFeature } from './parse.js';
 
 export {DataTable} from './DataTable.js';
 export {StepRegistry} from './StepRegistry.js';
 
 export async function generateSpec(inputPath: string, outputPath: string) {
   const feature = await readFile(inputPath).then(res=>res.toString('utf8'));
-  const code = generateCode(inputPath, feature);
+  const code = generateCode(parseFeature(inputPath, feature));
   await writeFile(outputPath, code);
 }
 
@@ -16,7 +17,7 @@ export async function generateSpecs(inputGlob: string) {
   const inputPaths = glob.sync(inputGlob);
   for (const inputPath of inputPaths) {
     const feature = await readFile(inputPath).then(res=>res.toString('utf8'));
-    const code = generateCode(inputPath, feature);
+    const code = generateCode(parseFeature(inputPath, feature));
     await writeFile(inputPath+'.js', code);
   }
 }
