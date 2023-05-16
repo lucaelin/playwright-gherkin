4d6a09500ac8e0228602eb376f7bd1f9f1f4d7d2
Luca Haneklau <git@luca.lsys.ac>
2023-05-16 18:46:24 +0200

    fix glob import

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import {readFile, writeFile} from 'node:fs/promises';
-import glob from 'glob';
-import {generateCode} from './generate.js';
-import {createHash} from 'node:crypto';
+import { readFile, writeFile } from 'node:fs/promises';
+import { glob } from 'glob';
+import { generateCode } from './generate.js';
+import { createHash } from 'node:crypto';
 import { parseFeature } from './parse.js';
 import { generateDeclaration } from './declaration.js';
 
-export {DataTable} from './DataTable.js';
-export {StepRegistry} from './StepRegistry.js';
+export { DataTable } from './DataTable.js';
+export { StepRegistry } from './StepRegistry.js';
 
 export async function generateSpec(inputPath: string) {
   const feature = await readFile(inputPath).then(res=>res.toString('utf8'));
